eca865f44abe4c9ac5cc44fdb19ba5092f459369
Noah Rissatti <noah.rissatii@gmail.com>
2023-04-25 09:22:04 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './paginas/Home/home';
+import Projetos from './paginas/Projetos/projetos';
+import Equipe from './paginas/Equipe/equipe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home></Home>} />
+      <Route path='/projetos' element={<Projetos></Projetos>} />
+      <Route path='/equipe' element={<Equipe></Equipe>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Equipe/equipe.js
@@ -0,0 +1,11 @@
+import Footer from "../../componentes/Footer/footer";
+import MenuNavegacao from "../../componentes/MenuNavegacao/menuNavegacao";
+
+export default function Equipe(){
+    return(
+        <div>
+            <MenuNavegacao></MenuNavegacao>
+            <Footer></Footer>
+        </div>
+    )
+}
