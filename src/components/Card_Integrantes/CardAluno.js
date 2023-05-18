aaee4da38b30e6b2e68f683bb99fc5c4b42281d1
Eduardo DD <dudu.derisso@gmail.com>
2023-05-18 13:11:33 -0300

    fix[CardAlunos,page Equipe]: layout e desposicao modificados
    foi modificado o espaçamento entre titulos e cards, o layout dos cards
    foi melhorado, mais foto de exemplo foram incrementados

--- src/components/Card_Integrantes/CardAluno.js
+++ src/components/Card_Integrantes/CardAluno.js
@@ -1,6 +1,6 @@
 import styles from './CardAluno.module.css'
 
-function CardAluno({nome, srcFoto, ano}) {
+function CardAluno({nome, srcFoto, ano, lattes, mail, github}) {
     
 
     function Evento() {
@@ -9,13 +9,17 @@ function CardAluno({nome, srcFoto, ano}) {
 
 
 
-    var altFoto =`Foto de ${nome}, integrante do PET ADS IFSP-Sao Carlos`
+    var altFoto =`Foto de ${nome}, integrante do PET ADS do IFSP Sao Carlos`
     return(
         <>
             <div className={styles.card}>
                 <img onMouseOver={Evento} className={styles.img} src={srcFoto} alt={altFoto}/>
-                <p className={styles.p}><b>Nome:</b> {nome} </p>
-                <p className={styles.p}><b>Ingresso:</b> {ano} </p>
+                <div className={styles.descricao}>
+                    <p className={styles.p}> {nome} </p>
+                    <p className={styles.p}><b>Ingresso:</b> {ano} </p>
+                </div>
+                <div className={styles.icons}>
+                </div>
             </div>
         </>
     )
--- src/pages/Equipe/equipe.js
+++ src/pages/Equipe/equipe.js
@@ -8,23 +8,22 @@ export default function Equipe() {
             <MenuNavegacao></MenuNavegacao>
             <div className="equipe--corpo">
 
-                <h2 className="equipe--titulo">Tutores:</h2>
+                <h2 className="equipe--titulo">Tutor(es):</h2>
                 <section className="equipe--tutores">
                     <CardAluno nome="Sr. Breno Duarte" srcFoto="http://4.bp.blogspot.com/_8BBWUsjhbHQ/TH6tsnB5zfI/AAAAAAAACrQ/3A8M7bbJRTI/s320/the-karate-kid2010jackie-chancatching-the-fly-with.jpg" ano="2022"/>
-                    <CardAluno nome="Dr. Benjamin da Conceição" srcFoto="http://4.bp.blogspot.com/_8BBWUsjhbHQ/TH6tsnB5zfI/AAAAAAAACrQ/3A8M7bbJRTI/s320/the-karate-kid2010jackie-chancatching-the-fly-with.jpg" ano="2022"/>
+                    <CardAluno nome="Dr. Benjamin da Conceição" srcFoto="https://i.pinimg.com/474x/11/aa/df/11aadfacde157057c67d6489e9abe9f1.jpg" ano="2022"/>
                 </section>
                     
                 <h2 className="equipe--titulo">Alunos:</h2>
                 <section className="equipe--alunos">
-                    <CardAluno nome="Srta. Stella Fernandes" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Vicente Cavalcanti" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Nicole Dias" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Alana Dias" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Dr. Emanuel Monteiro" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Luiz Gustavo Gomes Gustavo Gomeso" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="João Alícia Alves" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Thales Moreira" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
-                    <CardAluno nome="Ana Laura Barros" srcFoto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJVh5x--C3pPWaAFOHsTlxb4KIszVWmMNIS7UyLY9&s" ano="2022"/>
+                    <CardAluno nome="Srta. Stella Fernandes" srcFoto="https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Ootp076.jpg/220px-Ootp076.jpg" ano="2022"/>
+                    <CardAluno nome="Vicente Cavalcanti" srcFoto="https://cdn.britannica.com/11/215011-050-3127A07E/American-actor-Keanu-Reeves-2014.jpg" ano="2022"/>
+                    <CardAluno nome="Nicole Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg/220px-Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg" ano="2022"/>
+                    <CardAluno nome="Alana Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Naomie_Harris_2014.jpg/220px-Naomie_Harris_2014.jpg" ano="2022"/>
+                    <CardAluno nome="Dr. Emanuel Monteiro" srcFoto="https://cdn.britannica.com/77/191077-050-63262B99/Samuel-L-Jackson.jpg?w=400&h=300&c=crop" ano="2022"/>
+                    <CardAluno nome="Nicole Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg/220px-Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg" ano="2022"/>
+                    <CardAluno nome="Alana Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Naomie_Harris_2014.jpg/220px-Naomie_Harris_2014.jpg" ano="2022"/>
+                    <CardAluno nome="Dr. Emanuel Monteiro" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg/300px-Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg" ano="2022"/>
                 </section>
             </div>                                
         </>
