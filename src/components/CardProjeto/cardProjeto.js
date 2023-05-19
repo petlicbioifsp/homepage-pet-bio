a5bad20ed00f26176c9c11d2c0038112b8b0267b
Noah Rissatti <noah.rissatii@gmail.com>
2023-05-19 14:14:05 -0300

    card for projects ready

--- src/components/CardProjeto/cardProjeto.js
+++ src/components/CardProjeto/cardProjeto.js
@@ -4,17 +4,22 @@ import logoProjeto from "../../resources/kisspng-computer-icons-directory-icon-d
 import "./cardProjeto.css";
 
 export default function CardProjeto(props){
+
     const[isOpen,setIsOpen] = useState(false);
 
     return(
-        <motion.div className="card" onClick={()=> setIsOpen(!isOpen)}>
+        <motion.div className={isOpen ? "card aberto" : "card"} onClick={()=> setIsOpen(!isOpen)}>
             <motion.img className="logo-projeto" alt="simbolo de uma pasta" 
             src={logoProjeto}></motion.img>
             <motion.h2 className="card-titulo" >{props.titulo}</motion.h2>
             <motion.p className="card-descricao" >{props.descricao}</motion.p>
             {isOpen && (
                 <motion.div>
-                    <p className='card--texto__espancao'>AAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                    <p className='card--texto__espancao'>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                    <div className='card--texto__envolvidos'>
+                        <p className='card--texto__espancao'>Envolvidos: </p>
+                        <p className='card--texto__espancao'>{props.envolvidos}</p>
+                    </div>
                 </motion.div>
             )}
         </motion.div>
--- src/pages/Projetos/projetos.js
+++ src/pages/Projetos/projetos.js
@@ -9,7 +9,18 @@ export default function Projetos() {
             <div className="menu-projetos--corpo">
                 <h1 className="menu-projetos--titulo">Projetos desenvolvidos pelo PET</h1>
                 <section className="menu-projetos">
-                    <CardProjeto titulo="Projeto 1" descricao="Lorem ipsum "></CardProjeto>
+                    <CardProjeto titulo="Projeto Personalidades da Computação" 
+                    descricao="Projeto desenvolvido com intuito de pesquisar e montar cartazes com personalidades importantes da computação"
+                    envolvidos="Laura Ranucci Luchiari, Vitor Bonelli, Luiz Chiquetano, Bianca "
+                    ></CardProjeto>
+                    <CardProjeto titulo="Projeto Estudo de React" 
+                    descricao="Projeto desenvolvido com intuito de pesquisar e montar cartazes com personalidades importantes da computação"
+                    envolvidos="Laura Ranucci Luchiari, Vitor Bonelli, Luiz Chiquetano, Bianca "
+                    ></CardProjeto>
+                    <CardProjeto titulo="Projeto Wecomp" 
+                    descricao="Projeto desenvolvido com intuito de pesquisar e montar cartazes com personalidades importantes da computação"
+                    envolvidos="Laura Ranucci Luchiari, Vitor Bonelli, Luiz Chiquetano, Bianca "
+                    ></CardProjeto>
                 </section>
             </div>
         </>
