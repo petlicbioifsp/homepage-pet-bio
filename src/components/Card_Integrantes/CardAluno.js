f51682a95315fe8d91e435a9db251729540f8d26
Otávio Pereira Lopes <otavio.pereira.lopes@gmail.com>
2023-05-18 23:10:58 -0300

    Implementei o uso do JSON, Tipo de aluno, ex alunos e tive de mudar o local das pastas de fotos.

--- src/components/Card_Integrantes/CardAluno.js
+++ src/components/Card_Integrantes/CardAluno.js
@@ -1,28 +1,33 @@
-import styles from './CardAluno.module.css'
+import React from 'react';
+import styles from './CardAluno.module.css';
 
-function CardAluno({nome, srcFoto, ano, lattes, mail, github}) {
-    
+function CardAluno({ nome, srcFoto,tipo ,ano, lattes, mail, github }) {
+  function Evento() {
+    console.log('Teste do evento');
+  }
 
-    function Evento() {
-        console.log('Teste do evento')
-    }
+  var altFoto = `Foto de ${nome}, integrante do PET ADS do IFSP Sao Carlos`;
 
-
-
-    var altFoto =`Foto de ${nome}, integrante do PET ADS do IFSP Sao Carlos`
-    return(
-        <>
-            <div className={styles.card}>
-                <img onMouseOver={Evento} className={styles.img} src={srcFoto} alt={altFoto}/>
-                <div className={styles.descricao}>
-                    <p className={styles.p}> {nome} </p>
-                    <p className={styles.p}><b>Ingresso:</b> {ano} </p>
-                </div>
-                <div className={styles.icons}>
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <div className={styles.card}>
+      <img
+        onMouseOver={Evento}
+        className={styles.img}
+        src={srcFoto}
+        alt={altFoto}
+      />
+      <div className={styles.descricao}>
+        <p className={styles.p}>{nome}</p>
+        <p className={styles.p}>
+          <b>Ingresso:</b> {ano}
+        </p> 
+        <p className={styles.p}>
+          <b>Tipo:</b> {tipo}
+        </p>
+      </div>
+      <div className={styles.icons}>{/* Adicione seus ícones aqui */}</div>
+    </div>
+  );
 }
 
-export default CardAluno
+export default CardAluno;
--- src/pages/Equipe/equipe.js
+++ src/pages/Equipe/equipe.js
@@ -1,32 +1,70 @@
+import React, { useState, useEffect } from 'react';
 import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
 import CardAluno from "../../components/Card_Integrantes/CardAluno";
 import './equipe.css';
+import CardExAluno from '../../components/Card_Integrantes/CardExAlunos';
+import CardTutor from '../../components/Card_Integrantes/CardTutor';
 
 export default function Equipe() {
-    return (
-        <>
-            <MenuNavegacao></MenuNavegacao>
-            <div className="equipe--corpo">
+  const [alunos, setAlunos] = useState([]);
+  const [tutores, setTutores] = useState([]);
+  const [exAlunos,setexAlunos] = useState([]);
 
-                <h2 className="equipe--titulo">Tutor(es):</h2>
-                <section className="equipe--tutores">
-                    <CardAluno nome="Sr. Breno Duarte" srcFoto="http://4.bp.blogspot.com/_8BBWUsjhbHQ/TH6tsnB5zfI/AAAAAAAACrQ/3A8M7bbJRTI/s320/the-karate-kid2010jackie-chancatching-the-fly-with.jpg" ano="2022"/>
-                    <CardAluno nome="Dr. Benjamin da Conceição" srcFoto="https://i.pinimg.com/474x/11/aa/df/11aadfacde157057c67d6489e9abe9f1.jpg" ano="2022"/>
-                </section>
-                    
-                <h2 className="equipe--titulo">Alunos:</h2>
-                <section className="equipe--alunos">
-                    <CardAluno nome="Srta. Stella Fernandes" srcFoto="https://upload.wikimedia.org/wikipedia/en/thumb/b/b9/Ootp076.jpg/220px-Ootp076.jpg" ano="2022"/>
-                    <CardAluno nome="Vicente Cavalcanti" srcFoto="https://cdn.britannica.com/11/215011-050-3127A07E/American-actor-Keanu-Reeves-2014.jpg" ano="2022"/>
-                    <CardAluno nome="Nicole Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg/220px-Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg" ano="2022"/>
-                    <CardAluno nome="Alana Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Naomie_Harris_2014.jpg/220px-Naomie_Harris_2014.jpg" ano="2022"/>
-                    <CardAluno nome="Dr. Emanuel Monteiro" srcFoto="https://cdn.britannica.com/77/191077-050-63262B99/Samuel-L-Jackson.jpg?w=400&h=300&c=crop" ano="2022"/>
-                    <CardAluno nome="Nicole Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg/220px-Amy_Adams_UK_Nocturnal_Animals_Premiere_%28cropped%29.jpg" ano="2022"/>
-                    <CardAluno nome="Alana Dias" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Naomie_Harris_2014.jpg/220px-Naomie_Harris_2014.jpg" ano="2022"/>
-                    <CardAluno nome="Dr. Emanuel Monteiro" srcFoto="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg/300px-Nicolas_Cage_-_66%C3%A8me_Festival_de_Venise_%28Mostra%29.jpg" ano="2022"/>
-                </section>
-            </div>                                
-        </>
-        
-    )
+  useEffect(() => {
+    const fetchIntegrantes = async () => {
+      try {
+        const response = await fetch('./integrantes.json');
+        const data = await response.json();
+        setAlunos(data.aluno);
+        setTutores(data.tutores);
+        setexAlunos(data.exAlunos);
+      } catch (error) {
+        console.error('Ocorreu um erro ao obter os integrantes:', error);
+      }
+    };
+
+    fetchIntegrantes();
+  }, []);
+
+  return (
+    <>
+      <MenuNavegacao />
+      <div className="equipe--corpo">
+        <h2 className="equipe--titulo">Tutor:</h2>
+        <section className="equipe--tutores">
+          {tutores.map((tutor) => (
+            <CardTutor
+              key={tutor.nome}
+              nome={tutor.nome}
+              srcFoto={tutor.foto}
+              ano="2022"
+            />
+          ))}
+        </section>
+        <h2 className="equipe--titulo">Alunos:</h2>
+        <section className="equipe--alunos">
+          {alunos.map((aluno) => (
+            <CardAluno
+              key={aluno.id}
+              nome={aluno.nome}
+              srcFoto={aluno.foto}
+              tipo={aluno.tipo}
+              ano={aluno.ano}
+            />
+          ))}
+        </section>
+        <h2 className="equipe--titulo">Ex Alunos:</h2>
+        <section className="equipe--alunos">
+          {exAlunos.map((exAlunos) => (
+            <CardExAluno
+              key={exAlunos.id}
+              nome={exAlunos.nome}
+              srcFoto={exAlunos.foto}
+              anos={exAlunos.anos}
+            />
+          ))}
+        </section>
+      </div>
+    </>
+  );
 }
