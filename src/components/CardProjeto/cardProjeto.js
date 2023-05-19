c36396451f20dab30326c5cf18ed03357d1f8e23
Noah Rissatti <noah.rissatii@gmail.com>
2023-05-19 11:26:54 -0300

    making responsive cards for projects

--- src/components/CardProjeto/cardProjeto.js
+++ src/components/CardProjeto/cardProjeto.js
@@ -1,13 +1,22 @@
+import {motion} from 'framer-motion';
+import {useState} from 'react';
 import logoProjeto from "../../resources/kisspng-computer-icons-directory-icon-design-folder-icon-5b23828b1c8d19.801150631529053835117.png";
 import "./cardProjeto.css";
 
 export default function CardProjeto(props){
+    const[isOpen,setIsOpen] = useState(false);
+
     return(
-        <div className="card">
-            <img className="logo-projeto" alt="simbolo de uma pasta" 
-            src={logoProjeto}></img>
-            <h3 className="card-titulo">{props.titulo}</h3>
-            <p className="card-descricao">{props.descricao}</p>
-        </div>
+        <motion.div className="card" onClick={()=> setIsOpen(!isOpen)}>
+            <motion.img className="logo-projeto" alt="simbolo de uma pasta" 
+            src={logoProjeto}></motion.img>
+            <motion.h2 className="card-titulo">{props.titulo}</motion.h2>
+            <motion.p className="card-descricao">{props.descricao}</motion.p>
+            {isOpen && (
+                <motion.div>
+                    <p className='card--texto__espancao'>AAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                </motion.div>
+            )}
+        </motion.div>
     )
 }
