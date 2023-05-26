087f91446f5fc1edb2f3764d96a5a02cc66cd204
Noah Rissatti <noah.rissatii@gmail.com>
2023-05-26 14:33:53 -0300

    fix the github icon

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import {BsGithub} from 'react-icons/bs';
 export default function Footer(){
     return(
         <div className='footer'>
-            <a className='gitIcon' href='https://github.com/pet-ads' ><BsGithub/></a>
+            <a className='gitIcon' href='https://github.com/pet-ads' target="_blank"><BsGithub/></a>
             <p className="footer_content">Desenvolvido com ♥ pela equipe do PET/ADS - IFSP São Carlos - 2023</p>
         </div>
     )
