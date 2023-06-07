0af8592316d1936c0f82c7b0402db8aff7c65168
Fabio270 <81870980+Fabio270@users.noreply.github.com>
2023-06-06 22:49:05 -0300

    fix: scrollArrow
    Arrumei a setinha GLÓRIA

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,26 +1,47 @@
 import "./home.css"
-
+import React, { useState, useRef } from "react";
 import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
 import Info from "../../components/Home_Infos/Infos";
 import Title from "../../components/Home_title/Title";
 import Reveal from "../../components/Efeito Reveal/Reveal";
 import Button from "../../components/Home_button/Button";
-import ScrollArrow from '../../components/home_scroll_arrow/ScrollArrow.js';
 import LogoPET from "../../components/LogoPET/LogoPET";
 export default function Home() {
+    const [showArrow, setShowArrow] = useState(true);
+
+    const checaAltura = () => {
+        if (window.scrollY > window.innerHeight * 0.5) {
+            setShowArrow(false);
+        } else {
+            setShowArrow(true);
+        }
+    };
+
+    window.addEventListener('scroll', checaAltura);
+
+    const sectionRef = useRef(null);
+
+    const scrollToSection = () => {
+        sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+
     return (
         <div>
+                <div className={`scroll-arrow ${showArrow ? 'visible' : ''}`} onClick={scrollToSection}>
+                    <span className="arrow"></span>
+                </div>
             <div className="home">
                 <MenuNavegacao></MenuNavegacao>
                 <div className="headerContender">
                     <Title
                         title="Bem-vindo à página do PET/ADS @ IFSP São Carlos"
-                        content="Programa de Educação Tutorial"/>  
-                        <LogoPET/>
+                        content="Programa de Educação Tutorial" />
+                    <LogoPET />
 
                 </div>
 
                 <Reveal>
+                    <div ref={sectionRef}></div>
                     <Info
                         title="Sobre o PET"
                         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
@@ -31,9 +52,10 @@ export default function Home() {
                     Nullam nec eleifend purus, non ullamcorper turpis.
                     Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.. comes from a line in section 1.10.32."
                     />
-                    <Button
-                        value="Processo Seletivo"
-                        to='/processo-seletivo'/>
+                    <div>
+                        <Button value="Processo Seletivo"
+                            to='/processo-seletivo' />
+                    </div>
                 </Reveal>
 
                 <Reveal>
@@ -53,8 +75,6 @@ export default function Home() {
                     />
                 </Reveal>
             </div>
-            <ScrollArrow />
-
         </div>
     )
 }
