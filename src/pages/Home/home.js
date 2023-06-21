5a5474798c17922694d62d8fe75d74021ffc5b0e
lucas-ifsp <lucas.ruas@gmail.com>
2023-06-21 18:02:53 -0300

    atualização dos textos da página inicial e melhoria de css

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,10 @@
-import "./home.css"
-import React, { useState, useRef } from "react";
-import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
-import Info from "../../components/Home_Infos/Infos";
-import Title from "../../components/Home_title/Title";
+import React, { useRef, useState } from "react";
 import Reveal from "../../components/Efeito Reveal/Reveal";
+import Info from "../../components/Home_Infos/Infos";
 import Button from "../../components/Home_button/Button";
+import Title from "../../components/Home_title/Title";
+import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
+import "./home.css";
 export default function Home() {
     const [showArrow, setShowArrow] = useState(true);
 
@@ -33,7 +33,7 @@ export default function Home() {
                 <MenuNavegacao></MenuNavegacao>
                 <div className="headerContender">
                     <Title
-                        title="Bem-vindo à página do PET/ADS @ IFSP São Carlos"
+                        title="Bem-vindo à página do PET/ADS @ IFSP"
                         content="Programa de Educação Tutorial" />
 
                 </div>
@@ -41,14 +41,15 @@ export default function Home() {
                 <Reveal>
                     <Info
                         title="Sobre o PET"
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Aenean magna quam, aliquet id tortor a, facilisis faucibus erat. Nunc pretium tempor commodo. 
-                    Sed egestas, tortor id tincidunt pretium, nulla lectus molestie quam, in ornare elit magna et lacus.
-                     Integer aliquet feugiat lacus a rutrum. Praesent sed tempor nisl, sit amet cursus libero. 
-                    Etiam vel est bibendum, pulvinar neque ac, convallis lacus. 
-                    Nullam nec eleifend purus, non ullamcorper turpis.
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.. comes from a line in section 1.10.32."
-                    />
+                        content='O Programa de Educação Tutorial (PET) é um programa do governo federal brasileiro de estímulo a 
+                        atividades de pesquisa, ensino e extensão em nível de graduação. O PET foi criado e implantado em 1979 
+                        pela Coordenação de Aperfeiçoamento de Pessoal de Nível Superior - CAPES, com o nome original de Programa 
+                        Especial de Treinamento, cujo objetivo era criar um grupo de elite intelectual, dedicado à pesquisa e ao 
+                        estudo. Em 2004, o nome foi alterado para Programa de Educação Tutorial e adotou uma nova filosofia. Os 
+                        objetivos do programa, de acordo com o SESu, são a melhoria do ensino de graduação, a formação acadêmica 
+                        do aluno, a interdisciplinaridade, e o planejamento e execução de atividades acadêmicas diversificadas 
+                        em grupos de tutoria. De 2006 a 2012 ocorreu uma grande expansão dos grupos PET no país. Atualmente, existem
+                        842 grupos distribuídos em 121 Instituições de Ensino Superior.'/>
                     <div>
                         <Button value="Processo Seletivo"
                             to='/processo-seletivo' />
@@ -56,19 +57,19 @@ export default function Home() {
                 </Reveal>
 
                 <Reveal>
-                    
                     <Info
-                        title="Propósito do PET/ADS"
-                        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                    Aenean magna quam, aliquet id tortor a, facilisis faucibus erat. Nunc pretium tempor commodo. 
-                    Sed egestas, tortor id tincidunt pretium, nulla lectus molestie quam, in ornare elit magna et lacus.
-                    Integer aliquet feugiat lacus a rutrum. Praesent sed tempor nisl, sit amet cursus libero. 
-                    Etiam vel est bibendum, pulvinar neque ac, convallis lacus. 
-                    Nullam nec eleifend purus, non ullamcorper turpis.
-                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of de Finibus Bonorum et Malorum (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, Lorem ipsum dolor sit amet.. comes from a line in section 1.10.32.">
+                        title="Propósito"
+                        content='O grupo PET do curso de Análise e Desenvolvimento de Sistemas do IFSP São Carlos (PET/ADS/SCL) 
+                        foi criado em 2010. Desde sua criação, o PET/ADS/SCL já desenvolveu diversas atividades voltadas 
+                        ao ensino, à pesquisa e à extensão. O grupo é formado por um docente tutor, até doze bolsistas e até seis 
+                        voluntários. Cada estudante do PET possui uma dedidação semanal de 20h e atua simultaneamente em diferentes
+                        atividades, que vão desde o estudo de técnicas de Computação, linguagens e frameworks, até o desenvolvimento
+                        de sistemas e a organização de eventos acadêmicos. Os estudantes também se envolvem em projetos de extensão e
+                        de pesquisa tanto locais quanto em parceria com outras instituições de ensino. Entre 2010 e 2022, o PET/ADS/SCL
+                        teve como tutor o prof. João Luiz Franco. Atualmente, o grupo é coordenado pelo prof. Lucas Oliveira. '>
                     </Info>
                     <Button
-                        value="Ver Projetos"
+                        value="Conheça os Projetos"
                         to='/projetos'
                     />
                 </Reveal>
