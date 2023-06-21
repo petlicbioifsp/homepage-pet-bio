6492cff92c635856f0ae3bbac00409620d8bb025
lucas-ifsp <lucas.ruas@gmail.com>
2023-06-21 17:31:59 -0300

    melhoria no tamanho da fonte do FAQ e alinhamento do título

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -1,51 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Collapsible from 'react-collapsible';
+import CollapsibleOpen from "../../components/Faq/CollapsibleOpen";
+import CollapsibleTrigger from "../../components/Faq/CollapsibleTrigger";
 import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
-import Collapsible from 'react-collapsible'
-import "./Faq.css"
-import CollapsibleTrigger from "../../components/Faq/CollapsibleTrigger"
-import CollapsibleOpen from "../../components/Faq/CollapsibleOpen"
+import "./Faq.css";
 
 
 export default function Faq() {
-    const [arrFaq, setFaq] = useState([]);
+  const [arrFaq, setFaq] = useState([]);
 
-    useEffect(() => {
-        const fetchFaq = async () => {
-          try {
-            const response = await fetch('./faq.json');
-            const data = await response.json();
-        
-            setFaq(data.faqs);
-            console.log(data.faqs)
-          } catch (error) {
-            console.error('Ocorreu um erro ao obter o FAQ:', error);
-          }
-        };
-    
-        fetchFaq();
-      }, []);
+  useEffect(() => {
+    const fetchFaq = async () => {
+      try {
+        const response = await fetch('./faq.json');
+        const data = await response.json();
 
-    return (
+        setFaq(data.faqs);
+        console.log(data.faqs)
+      } catch (error) {
+        console.error('Ocorreu um erro ao obter o FAQ:', error);
+      }
+    };
+    fetchFaq();
+  }, []);
 
-        <body>
-            <MenuNavegacao></MenuNavegacao>
+  return (
 
-            <h2 className="faq-titulo">Perguntas frequentes</h2>
-            <section className="faq--caixa__perguntas">
+    <body>
+      <MenuNavegacao></MenuNavegacao>
 
-                {arrFaq.map((faqs) => (
-                <Collapsible 
-                trigger={<CollapsibleTrigger text={faqs.trigger} />}
-                triggerWhenOpen={<CollapsibleOpen text={faqs.trigger} />}
-                >
-                    <p className="faq-resposta">{faqs.resposta}</p>
-                </Collapsible>
-                
-                ))} 
+      <h2 className="faq-titulo">Perguntas frequentes</h2>
+      <section className="faq--caixa__perguntas">
 
-            </section>
+        {arrFaq.map((faqs) => (
+          <Collapsible
+            trigger={<CollapsibleTrigger text={faqs.trigger} />}
+            triggerWhenOpen={<CollapsibleOpen text={faqs.trigger} />}
+          >
+            <p className="faq-resposta">{faqs.resposta}</p>
+          </Collapsible>
+
+        ))}
+
+      </section>
 
 
-        </body>
-    )
+    </body>
+  )
 }
