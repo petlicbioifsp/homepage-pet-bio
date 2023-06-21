47193edd8e763b4342c0804e8c531e6d579f6f46
lucas-ifsp <lucas.ruas@gmail.com>
2023-06-21 18:55:10 -0300

    Solve merge conflicts

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,10 +1,10 @@
+import { BsGithub } from 'react-icons/bs';
 import './footer.css';
-import {BsGithub} from 'react-icons/bs';
 
-export default function Footer(){
-    return(
-        <footer className='footer'> 
-            <a className='gitIcon' href='https://github.com/pet-ads'rel= "noreferrer" target="_blank"><BsGithub/></a>
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <a className='gitIcon' href='https://github.com/pet-ads' rel="noreferrer" target="_blank"><BsGithub /></a>
             <p className="footer_content">Desenvolvido com ♥ por PET/ADS @ IFSP São Carlos.</p>
         </footer>
     )
--- src/components/Home_title/Title.js
+++ src/components/Home_title/Title.js
@@ -1,13 +1,13 @@
-import styles from './Title.module.css'
 import LogoPET from '../LogoPET/LogoPET';
-function Title({title, content}){
-    return(
+import styles from './Title.module.css';
+function Title({ title, content }) {
+    return (
         <header className={styles.cabecalho}>
-        <div>
-        <h1 className={styles.title}>{title}</h1>
-        <h2 className={styles.subtitle}>{content}</h2> 
-       </div>
-        <LogoPET />
+            <div>
+                <h1 className={styles.title}>{title}</h1>
+                <h2 className={styles.subtitle}>{content}</h2>
+            </div>
+            <LogoPET />
         </header>
     )
 } export default Title;
