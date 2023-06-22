7396b84d8d862e27b0a7b69126d0fd81c46a99f5
lucas-ifsp <lucas.ruas@gmail.com>
2023-06-21 21:17:13 -0300

    adiciona texto real nos cards de projeto e altera as props para contemplar texto curto e longo

--- src/components/CardProjeto/cardProjeto.js
+++ src/components/CardProjeto/cardProjeto.js
@@ -1,24 +1,23 @@
-import {motion} from 'framer-motion';
-import {useState} from 'react';
+import { motion } from 'framer-motion';
+import { useState } from 'react';
 import logoProjeto from "../../resources/kisspng-computer-icons-directory-icon-design-folder-icon-5b23828b1c8d19.801150631529053835117.png";
 import "./cardProjeto.css";
 
-export default function CardProjeto(props){
+export default function CardProjeto(props) {
 
-    const[isOpen,setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <motion.div className={isOpen ? "card aberto" : "card"} onClick={()=> setIsOpen(!isOpen)}>
-            <motion.img className="logo-projeto" alt="simbolo de uma pasta" 
-            src={logoProjeto}></motion.img>
+    return (
+        <motion.div className={isOpen ? "card aberto" : "card"} onClick={() => setIsOpen(!isOpen)}>
+            <motion.img className="logo-projeto" alt="simbolo de uma pasta" src={logoProjeto}></motion.img>
             <motion.h2 className="card-titulo" >{props.titulo}</motion.h2>
-            <motion.p className="card-descricao" >{props.descricao}</motion.p>
+            {!isOpen && <motion.p className="card-descricao" >{props.descricao}</motion.p>}
             {isOpen && (
                 <motion.div>
-                    <p className='card--texto__espancao'>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+                    <p className='card--texto__expandido'>{props.expandido}</p>
                     <div className='card--texto__envolvidos'>
-                        <p className='card--texto__espancao'>Envolvidos: </p>
-                        <p className='card--texto__espancao'>{props.envolvidos}</p>
+                        <p className='card--texto__expandido'>Envolvidos: </p>
+                        <p className='card--texto__expandido'></p>
                     </div>
                 </motion.div>
             )}
