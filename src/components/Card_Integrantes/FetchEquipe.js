5b03b0de55b2c14158f8c09b705fdd0ec828d7c1
Otávio Pereira Lopes <otavio.pereira.lopes@gmail.com>
2023-06-26 10:53:52 -0300

    Transformei o fetch da equipe em um componente.

---
+++ src/components/Card_Integrantes/FetchEquipe.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+const FetchEquipe = ({ render }) => {
+  const [alunos, setAlunos] = useState([]);
+  const [tutores, setTutores] = useState([]);
+  const [exAlunos, setExAlunos] = useState([]);
+
+  useEffect(() => {
+    const fetchIntegrantes = async () => {
+      try {
+        const response = await fetch('./integrantes.json');
+        const data = await response.json();
+        setAlunos(data.aluno);
+        setTutores(data.tutores);
+        setExAlunos(data.exAlunos);
+      } catch (error) {
+        console.error('Ocorreu um erro ao obter os integrantes:', error);
+      }
+    };
+
+    fetchIntegrantes();
+  }, []);
+
+  return render({ alunos, tutores, exAlunos });
+};
+
+export default FetchEquipe;
--- src/pages/Equipe/equipe.js
+++ src/pages/Equipe/equipe.js
@@ -1,36 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
-import CardAluno from "../../components/Card_Integrantes/CardAluno";
+import React, { useState } from 'react';
+import MenuNavegacao from '../../components/MenuNavegacao/menuNavegacao';
+import CardAluno from '../../components/Card_Integrantes/CardAluno';
 import './equipe.css';
 import CardExAluno from '../../components/Card_Integrantes/CardExAlunos';
 import CardTutor from '../../components/Card_Integrantes/CardTutor';
-import CollapsibleTrigger from '../Equipe/CollapsibleTrigger';
-import CollapsibleOpen from '../Equipe/CollabsibleOpen';
-import Collapsible from 'react-collapsible';
-
+import CollapsibleOpen from '../../components/Faq/CollapsibleOpen';
+import CollapsibleTrigger from '../../components/Faq/CollapsibleTrigger';
+import FetchEquipe from '../../components/Card_Integrantes/FetchEquipe';
 
 export default function Equipe() {
-  const [alunos, setAlunos] = useState([]);
-  const [tutores, setTutores] = useState([]);
-  const [exAlunos, setExAlunos] = useState([]);
   const [showExAlunos, setShowExAlunos] = useState(false);
-
-  useEffect(() => {
-    const fetchIntegrantes = async () => {
-      try {
-        const response = await fetch('./integrantes.json');
-        const data = await response.json();
-        setAlunos(data.aluno);
-        setTutores(data.tutores);
-        setExAlunos(data.exAlunos);
-      } catch (error) {
-        console.error('Ocorreu um erro ao obter os integrantes:', error);
-      }
-    };
-
-    fetchIntegrantes();
-  }, []);
-
   const toggleExAlunos = () => {
     setShowExAlunos(!showExAlunos);
   };
@@ -39,45 +18,51 @@ export default function Equipe() {
     <>
       <MenuNavegacao />
       <div className="equipe--corpo">
-        <h2 className="equipe--titulo">Integrantes</h2>
-        <section className="equipe--alunos">
-          {tutores.map((tutor) => (
-            <CardTutor
-              key={tutor.id}
-              nome={tutor.nomeTutor}
-              srcFoto={tutor.foto}
-              ano={tutor.ano}
-            />
-          ))}
-          {alunos.map((aluno) => (
-            <CardAluno
-              key={aluno.id}
-              nome={aluno.nome}
-              srcFoto={aluno.foto}
-              tipo={aluno.tipo}
-              ano={aluno.ano}
-              github={aluno.github}
-              linkedin={aluno.linkedin}
-            />
-          ))}
-        </section>
-        <h2 className="equipe--titulo">Ex Integrantes</h2>
-        <CollapsibleTrigger
-          text="Mostrar ex-integrantes"
-          onClick={toggleExAlunos}
-        />
-        {showExAlunos && (
-          <section className="equipe--alunos">
-            {exAlunos.map((exAluno) => (
-              <CardExAluno
-                key={exAluno.id}
-                nome={exAluno.nome}
-                srcFoto={exAluno.foto}
-                anos={exAluno.anos}
+        <FetchEquipe
+          render={({ alunos, tutores, exAlunos }) => (
+            <>
+              <h2 className="equipe--titulo">Integrantes</h2>
+              <section className="equipe--alunos">
+                {tutores.map((tutor) => (
+                  <CardTutor
+                    key={tutor.id}
+                    nome={tutor.nomeTutor}
+                    srcFoto={tutor.foto}
+                    ano={tutor.ano}
+                  />
+                ))}
+                {alunos.map((aluno) => (
+                  <CardAluno
+                    key={aluno.id}
+                    nome={aluno.nome}
+                    srcFoto={aluno.foto}
+                    tipo={aluno.tipo}
+                    ano={aluno.ano}
+                    github={aluno.github}
+                    linkedin={aluno.linkedin}
+                  />
+                ))}
+              </section>
+              <h2 className="equipe--titulo">Ex Integrantes</h2>
+              <CollapsibleTrigger
+                text="Mostrar ex-integrantes"
+                onClick={toggleExAlunos}
               />
-            ))}
-          </section>
-        )}
+              {showExAlunos && (
+                <section className="equipe--alunos">
+                  {exAlunos.map((exAluno) => (
+                    <CardExAluno
+                      key={exAluno.id}
+                      nome={exAluno.nome}
+                      srcFoto={exAluno.foto}
+                      anos={exAluno.anos}
+                    />
+                  ))}
+                </section>
+              )}
+            </>
+          )}
+        />
       </div>
     </>
   );
