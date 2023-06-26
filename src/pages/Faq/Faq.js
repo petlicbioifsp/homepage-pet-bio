89530e73a12153f147a7e1a941214cef7df23c5a
Otávio Pereira Lopes <otavio.pereira.lopes@gmail.com>
2023-06-26 11:31:36 -0300

    Transformei o fecth de FAQ em um componente unico.

--- src/pages/Faq/Faq.js
+++ src/pages/Faq/Faq.js
@@ -1,43 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Collapsible from 'react-collapsible';
-import CollapsibleOpen from "../../components/Faq/CollapsibleOpen";
-import CollapsibleTrigger from "../../components/Faq/CollapsibleTrigger";
-import MenuNavegacao from "../../components/MenuNavegacao/menuNavegacao";
-import "./Faq.css";
-
+import CollapsibleOpen from '../../components/Faq/CollapsibleOpen';
+import CollapsibleTrigger from '../../components/Faq/CollapsibleTrigger';
+import MenuNavegacao from '../../components/MenuNavegacao/menuNavegacao';
+import FetchFaq from '../../components/Faq/FetchFAQ';
+import './Faq.css';
 
 export default function Faq() {
-  const [arrFaq, setFaq] = useState([]);
-
-  useEffect(() => {
-    const fetchFaq = async () => {
-      try {
-        const response = await fetch('./faq.json');
-        const data = await response.json();
-        setFaq(data.faqs);
-      } catch (error) {
-        console.error('Ocorreu um erro ao obter o FAQ:', error);
-      }
-    };
-    fetchFaq();
-  }, []);
-
   return (
     <body>
       <MenuNavegacao />
       <h2 className="faq-titulo">Perguntas Frequentes</h2>
       <section className="faq--caixa__perguntas">
-
-        {arrFaq.map((faqs) => (
-          <Collapsible
-            trigger={<CollapsibleTrigger text={faqs.trigger} />}
-            triggerWhenOpen={<CollapsibleOpen text={faqs.trigger} />}
-          >
-            <p className="faq-resposta">{faqs.resposta}</p>
-          </Collapsible>
-        ))}
-
+        <FetchFaq
+          render={(arrFaq) =>
+            arrFaq.map((faqs) => (
+              <Collapsible
+                key={faqs.id}
+                trigger={<CollapsibleTrigger text={faqs.trigger} />}
+                triggerWhenOpen={<CollapsibleOpen text={faqs.trigger} />}
+              >
+                <p className="faq-resposta">{faqs.resposta}</p>
+              </Collapsible>
+            ))
+          }
+        />
       </section>
     </body>
-  )
+  );
 }
