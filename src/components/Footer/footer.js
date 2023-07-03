ab493b929ce71e3fd2f89dd0037b2f5880fb928a
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-07-03 17:25:06 -0300

    Adiciona links  ao footer e animação o Icone do Github .

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,11 +1,13 @@
-import { BsGithub } from 'react-icons/bs';
+import { FaGithub } from "react-icons/fa";
+import {Link} from "react-router-dom";
+import useScrollToTop from "../../pages/Home/useScrollToTop";
 import './footer.css';
 
 export default function Footer() {
     return (
         <footer className='footer'>
-            <a className='gitIcon' href='https://github.com/pet-ads' rel="noreferrer" target="_blank"><BsGithub /></a>
-            <p className="footer_content">Desenvolvido com ♥ por PET/ADS @ IFSP São Carlos.</p>
+            <a className='gitIcon' href='https://github.com/pet-ads' rel="noreferrer" target="_blank"><FaGithub /></a>
+            <pre className="footer_content">Desenvolvido com ♥ por <Link className="link"  to="/"  onClick={useScrollToTop}>PET/ADS</Link> <a className="link" href="https://scl.ifsp.edu.br"  rel ="noreferrer"target="_blank"> @ IFSP São Carlos</a>.</pre>
         </footer>
     )
 } 
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -32,8 +32,8 @@ export default function Home() {
                         pela Coordenação de Aperfeiçoamento de Pessoal de Nível Superior - CAPES, com o nome original de Programa 
                         Especial de Treinamento, cujo objetivo era criar um grupo de elite intelectual, dedicado à pesquisa e ao 
                         estudo. Em 2004, o nome foi alterado para Programa de Educação Tutorial e adotou uma nova filosofia. Os 
-                        objetivos do programa, de acordo com o SESu, são a melhoria do ensino de graduação, a formação acadêmica 
-                        do aluno, a interdisciplinaridade, e o planejamento e execução de atividades acadêmicas diversificadas 
+                        objetivos do programa, de acordo com a Secretaria de Educação Superior (SESu), são a melhoria do ensino de graduação, a formação acadêmica 
+                        do aluno, a interdisciplinaridade e o planejamento e execução de atividades acadêmicas diversificadas 
                         em grupos de tutoria. De 2006 a 2012 ocorreu uma grande expansão dos grupos PET no país. Atualmente, existem
                         842 grupos distribuídos em 121 Instituições de Ensino Superior.'/>
 
