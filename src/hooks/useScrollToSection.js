e64f52bff36a70805c82ccd923a0432b4bfc07d8
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-07-05 14:15:54 -0300

    Fix: refatorando hook useScrollToSection

--- src/hooks/useScrollToSection.js
+++ src/hooks/useScrollToSection.js
@@ -4,35 +4,31 @@ const useScrollToSection = () => {
   const [showArrow, setShowArrow] = useState(true);
   const sectionRef = useRef(null);
 
-  const checaAltura = () => {
-    if (window.scrollY > window.innerHeight * 0.5) {
-      setShowArrow(false);
-    }else{
-        setShowArrow(true);
-    }
-  };
-
   const scrollToSection = () => {
       sectionRef.current.scrollIntoView({ behavior: "smooth", block: "end" });
   };
+
   useEffect(() => {
-    const handleResize = () => {
-      if (window.matchMedia("(max-width: 400px)").matches) {
-        // Telas menores
-        checaAltura()
-      } else {
-        // Telas maiores
-        checaAltura();
+    const handleShowArrow = () => {
+
+      if (window.scrollY > window.innerHeight * 0.5) {
+        setShowArrow(false);
+
+      }else{
+          setShowArrow(true);
       }
     };
 
-    window.addEventListener('scroll', checaAltura);
-    window.addEventListener('resize', handleResize);
-
+      window.addEventListener('scroll',handleShowArrow);
+      window.addEventListener('resize', handleShowArrow);
+    
     return () => {
-      window.removeEventListener('scroll', checaAltura);
-      window.removeEventListener('resize', handleResize);
+    
+      window.removeEventListener('scroll',handleShowArrow);
+      window.removeEventListener('resize', handleShowArrow);
+    
     };
+  
   }, []);
 
   return { showArrow, sectionRef, scrollToSection };
