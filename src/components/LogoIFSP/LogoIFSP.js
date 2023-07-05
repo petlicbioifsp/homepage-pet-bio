99985caf49cf36fc8e485e03ba2c40f40de58f03
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-07-05 13:23:16 -0300

    Atualização de componentes

--- src/components/LogoIFSP/LogoIFSP.js
+++ src/components/LogoIFSP/LogoIFSP.js
@@ -2,7 +2,10 @@ import styles from './LogoIFSP.module.css'
 import IFSPLogo from '../../resources/LogoIfsp.png'
 function LogoIFSP (){
     return(
-        <img className={styles.IFSPLogo} src={IFSPLogo} alt='Identidade visual IFSP/SCL' />        
+        <a href='https://scl.ifsp.edu.br' rel="noreferrer" target="_blank">
+                 <img className={styles.IFSPLogo} src={IFSPLogo}  alt='Identidade visual IFSP/SCL'/>  
+        </a>
+
     )
 }
 export default LogoIFSP;
