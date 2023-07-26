6729b0f2223d13c98d517c4ce417b5138024cc81
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-07-26 17:14:22 -0300

    Adiciona link de download a página de processo seletivo

--- src/pages/ProcessoSeletivo/SubComponentes/Inscricoes/Inscricoes.js
+++ src/pages/ProcessoSeletivo/SubComponentes/Inscricoes/Inscricoes.js
@@ -6,11 +6,11 @@ function Inscricoes({titulo,texto_1,email,texto_2,data,estado,texto_3}){
         <div className={styles.Inscricao}>
             <h2 className={styles.titulo}>{titulo}</h2>
             <p className={styles.texto}>
-                {texto_1} <a className={styles.email} href="mailto:pet.ads.scl@ifsp.edu.br?subject=Processo seletivo PET/ADS"> {email}</a>.
+                {texto_1} <a className={styles.email} href="mailto:pet.ads.scl@ifsp.edu.br?subject=Processo seletivo PET/ADS">{email}</a>.
             </p>
              <p className={styles.texto}>{texto_2} <span className={styles.encerrado}>{data}</span> {estado}</p>
              <p className={styles.texto}>{texto_3} </p>
-             <p className={styles.download}><AiOutlineCloudDownload /></p>
+             <p className={styles.download}><a  href=' https://drive.google.com/file/d/1mRjpnZ5xrETw9uUEJDPvhFZNyDVl8Gv3/view' rel='noreferrer' target='_blank'><AiOutlineCloudDownload className={styles.btnDownload}/></a></p>
         </div>
     )
 }
