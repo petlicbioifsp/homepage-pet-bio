dcb91ded8cda0777fdfcd7368313b61d067ceba1
Otávio Pereira Lopes <otavio.pereira.lopes@gmail.com>
2023-08-16 11:31:23 -0300

    Colocando icone de linkedin para os ex-alunos.

--- src/pages/Equipe/SubComponentes/Card_Integrantes/CardExAlunos.js
+++ src/pages/Equipe/SubComponentes/Card_Integrantes/CardExAlunos.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 import styles from './CardExAluno.module.css';
 import { FaLinkedin } from 'react-icons/fa';
 
-function CardExAluno({ nome, srcFoto, linkedin }) {
-  function Evento() {
-    console.log('Teste do evento');
-  }
+const withBlur = { filter: "blur(2px) grayscale(80%) brightness(0.5)" };
+const noBlur = { filter: "blur(0px) grayscale(0%)" };
+
+function CardExAluno({ nome, srcFoto, tipo,linkedin }) {
+  const [blurred, setBlur] = useState(noBlur);
 
   var altFoto = `Foto de ${nome}, Ex integrante do PET ADS do IFSP Sao Carlos`;
 
   return (
     <div className={styles.card}>
+    <div
+      className={styles.imgContainer}
+      onMouseEnter={() => setBlur(withBlur)}
+      onMouseLeave={() => setBlur(noBlur)}
+    >
       <img
-        onMouseOver={Evento}
+        style={blurred}
         className={styles.img}
         src={srcFoto}
         alt={altFoto}
       />
-      <div className={styles.descricao}>
-        <p className={styles.p}>{nome}</p>
+      <div className={styles.iconWrapper}>
+        <a href={linkedin} target='_blank' rel='noopener noreferrer'>
+          <span className={styles.linkedinIcon}>
+            <FaLinkedin />
+          </span>
+        </a>
       </div>
     </div>
+
+    <div className={styles.descricao}>
+      <strong>
+        <p className={styles.p}>{nome}</p>
+      </strong>
+      <pre></pre>
+      <strong>
+        <p className={styles.p}>{tipo}</p>
+      </strong>
+    </div>
+  </div>
   );
 }
 
