a4e7c225f40d93f766eafb29974037cfcf98f251
Eduardo Duarte Derisso <dudu.derisso@gmail.com>
2023-08-16 12:06:08 -0300

    criado popUps em cards projetos

--- src/pages/Projetos/SubComponentes/CardProjeto/cardProjeto.js
+++ src/pages/Projetos/SubComponentes/CardProjeto/cardProjeto.js
@@ -9,46 +9,89 @@ import logoDesenvolvimento from "../../../../resources/cloud-coding.png";
 export default function CardProjeto(props) {
 
     const [isOpen, setIsOpen] = useState(false);
+    const linkIcon = document.querySelector(".logoConteiner>a")
+
+    function setingIsOpen() {
+        setIsOpen(!isOpen);
+    }
+
+    function removeOpeningEvent() {
+        linkIcon.removeEventListener('click', setingIsOpen);
+        alert("deu bom");
+    }
 
     return (
-        <motion.div className={isOpen ? "card aberto" : "card"}>
-            <motion.div className="logoConteiner">
-                {props.tipo === 'ensino' && <motion.img className= 'logo-projeto' src={logoEnsino} alt="ensino"/>}
-
-                {props.tipo === 'pesquisa' && <motion.img className= 'logo-projeto' src={logoPesquisa} alt="pesquisa" />}
-
-                {props.tipo === 'extensão' && <motion.img className = 'logo-projeto'  src={logoExtensao} alt="extensao" />}
-
-                {props.tipo === 'extensão-desenvolivmento' && <motion.img className = 'logo-projeto'  src={logoDesenvolvimento} alt="extensao-desenvolvimento" />}
-
-                <motion.a rel='noopener noreferrer' target="_blank" href={props.link}>
-                    <FaGithub className="Icon"/>
-                </motion.a>
-            
-            </motion.div>
-           
-           <motion.div onClick={() => setIsOpen(!isOpen)}>
-
+       <>
            {!isOpen && (
+            <motion.div 
+                className={isOpen ? "card aberto" : "card fechado"} 
+                onClick={setingIsOpen}
+            >
+
+                <motion.div className="logoConteiner">
+                    {props.tipo === 'ensino' && <motion.img className= 'logo-projeto' src={logoEnsino} alt="ensino"/>}
+                    {props.tipo === 'pesquisa' && <motion.img className= 'logo-projeto' src={logoPesquisa} alt="pesquisa" />}
+                    {props.tipo === 'extensão' && <motion.img className = 'logo-projeto'  src={logoExtensao} alt="extensao" />}
+                    {props.tipo === 'extensão-desenvolivmento' && <motion.img className = 'logo-projeto'  src={logoDesenvolvimento} alt="extensao-desenvolvimento" />}
+
+                    <motion.a 
+                        rel='noopener noreferrer' target="_blank" href={props.link}
+                        onClick={removeOpeningEvent}
+                    >
+                        <FaGithub className="Icon"/>
+                    </motion.a>
                 
-                <><motion.h2 className="card-titulo">{props.titulo}</motion.h2>
-                <motion.p className="card-descricao">{props.descricao}</motion.p></>)}
+                </motion.div>
+                
+                <motion.div >
+
+                        <motion.h2 className="card-titulo">{props.titulo}</motion.h2>
+                        <motion.p className="card-descricao">{props.descricao}</motion.p>
+                        
+                </motion.div>
+            </motion.div>
+                )}
                 
             {isOpen && (
-            <>
-                <motion.h2 className="card-titulo">{props.titulo} → {props.status}</motion.h2>
-                    <motion.div>
-                        <p className='card--texto__expandido'>{props.expandido}</p>
-                        <div className='card--texto__envolvidos'>
-                            <p className='card--texto__expandido'>{props.tecnologias} </p>
-                            <p className='card--texto__expandido'></p>
-                        </div>
-                </motion.div>
-            </>
-            )}
-           </motion.div>
+                <>
+                    <div className="card_background"></div>
+                    <motion.div 
+                        className={isOpen ? "card aberto" : "card fechado"}
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <motion.div className="logoConteiner">
+                            {props.tipo === 'ensino' && <motion.img className= 'logo-projeto' src={logoEnsino} alt="ensino"/>}
             
+                            {props.tipo === 'pesquisa' && <motion.img className= 'logo-projeto' src={logoPesquisa} alt="pesquisa" />}
+            
+                            {props.tipo === 'extensão' && <motion.img className = 'logo-projeto'  src={logoExtensao} alt="extensao" />}
+            
+                            {props.tipo === 'extensão-desenvolivmento' && <motion.img className = 'logo-projeto'  src={logoDesenvolvimento} alt="extensao-desenvolvimento" />}
+            
+                            <motion.a 
+                                rel='noopener noreferrer' target="_blank" href={props.link}
+                                onClick={removeOpeningEvent}
+                            >
+                                <FaGithub className="Icon"/>
+                            </motion.a>
+                        
+                        </motion.div>
+                
+                        <motion.div>
+                            <motion.h2 className="card-titulo">{props.titulo} → {props.status}</motion.h2>
+                            <motion.div  className="scrowSpace">
+                                <p className='card--texto__expandido'>{props.expandido}</p>
+                                <div className='card--texto__envolvidos'>
+                                    <p className='card--texto__expandido'>{props.tecnologias} </p>
+                                    <p className='card--texto__expandido'></p>
+                                </div>
+                            </motion.div>
+                    
+                        </motion.div>
 
-        </motion.div>
+                </motion.div>
+           </>
+            )}
+            </>
     )
 }
