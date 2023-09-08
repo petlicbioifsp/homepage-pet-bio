e0e06d88d2e02a901aaa7c84003a050067d0e6f3
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-09-08 14:00:52 -0300

    fix: fix  404 error on refresh
    Alterado <BrowserRouter> para <HashRouter> para solucionar o problema de 404 em refresh.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Home from './pages/Home/home';
 import Equipe from './pages/Equipe/equipe';
 import Footer from './components/Footer/footer';
 import Projetos from './pages/Projetos/projetos';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import ProcessoSeletivo from './pages/ProcessoSeletivo/ProcessoSeletivo';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter hashType = "noslash">
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -21,7 +21,7 @@ function App() {
         </Routes>
       </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
