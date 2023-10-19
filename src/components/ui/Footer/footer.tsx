d827fc2ac775aa3b1e1a0798c82590117d241080
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-10-19 11:19:48 -0300

    refactor: Improve readability on Footer component

--- src/components/ui/Footer/footer.tsx
+++ src/components/ui/Footer/footer.tsx
@@ -1,31 +1,37 @@
-import React from 'react';
+import React from "react";
 import styles from "./footer.module.css";
-import { FaGithub } from 'react-icons/fa';
-import Link from "../../interactive/Link/Link"
+import { FaGithub } from "react-icons/fa";
+import Link from "../../interactive/Link/Link";
 
-
-export default function footer(){
-    return(
+export default function Footer() {
+  return (
     <footer className={styles.footer}>
-    
-    <Link className="gitIcon" href='https://github.com/pet-ads' 
-        title='Visite nosso github' rel='noreferrer' target='_blank'>
-            <FaGithub />
-    </Link>
-  
-    <pre className={styles.footer_content}>
-        Desenvolvido com ♥ por{' '}
-  
-    <Link  className= "link" href='/' title='Página inicial'>
-      PET/ADS
-    </Link>{' '}
-  
-    @{' '}
-    <Link className = "link"href='https://scl.ifsp.edu.br' title='Página do IFSP-SCL' rel='noreferrer' target='_blank'>
-      IFSP São Carlos.
-    </Link>
-  
-  </pre>
-</footer>
-    )
+      <Link
+        className="gitIcon"
+        href="https://github.com/pet-ads"
+        title="Visite nosso github"
+        rel="noreferrer"
+        target="_blank"
+      >
+        <FaGithub />
+      </Link>
+
+      <pre className={styles.footer_content}>
+        Desenvolvido com ♥ por{" "}
+        <Link className="link" href="/" title="Página inicial">
+          PET/ADS
+        </Link>{" "}
+        @{" "}
+        <Link
+          className="link"
+          href="https://scl.ifsp.edu.br"
+          title="Página do IFSP-SCL"
+          rel="noreferrer"
+          target="_blank"
+        >
+          IFSP São Carlos.
+        </Link>
+      </pre>
+    </footer>
+  );
 }
