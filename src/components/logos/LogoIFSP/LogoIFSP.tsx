86a119ab92f9a63c8fff05a97e9b0d1332c0e46b
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-10-19 11:14:44 -0300

    refactor: Improves readability to Logo components

--- src/components/logos/LogoIFSP/LogoIFSP.tsx
+++ src/components/logos/LogoIFSP/LogoIFSP.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import styles from './LogoIFSP.module.css'
-import IFSPLogo from '../../../resources/logos/LogoIfsp.png'
-import Link from '../../interactive/Link/Link'
-function LogoIFSP (){
-    return(
-        <Link href='https://scl.ifsp.edu.br' rel="noreferrer" target="_blank">
-                 <img className={styles.IFSPLogo} src={IFSPLogo}  alt='Identidade visual IFSP/SCL'/>  
-        </Link>
+import React from "react";
+import styles from "./LogoIFSP.module.css";
+import IFSPLogo from "../../../resources/logos/LogoIfsp.png";
+import Link from "../../interactive/Link/Link";
 
-    )
+function LogoIFSP() {
+  return (
+    <Link href="https://scl.ifsp.edu.br" rel="noreferrer" target="_blank">
+      <img className={styles.IFSPLogo} src={IFSPLogo} alt="Identidade visual IFSP/SCL" />
+    </Link>
+  );
 }
+
 export default LogoIFSP;
--- src/components/logos/LogoPET/LogoPET.tsx
+++ src/components/logos/LogoPET/LogoPET.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import styles from './LogoPET.module.css'
-import Logo from '../../../resources/logos/LogoPET.png'
+import React from "react";
+import styles from "./LogoPET.module.css";
+import Logo from "../../../resources/logos/LogoPET.png";
 
-export default function LogoPET(){
-    return(
-        <img className={styles.PETLogo} src={Logo} alt='Identidade Visual PET/ADS'/>
-    )
-};
+export default function LogoPET() {
+  return <img className={styles.PETLogo} src={Logo} alt="Identidade Visual PET/ADS" />;
+}
