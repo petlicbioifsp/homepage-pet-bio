8b36b74e588e7b6f37b5a261152faabf781b549f
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-10-20 18:19:57 -0300

    Refactor: Improve the return type and readability of the Link component

--- src/components/interactive/Link/Link.tsx
+++ src/components/interactive/Link/Link.tsx
@@ -11,9 +11,9 @@ interface ILinkProps {
   children: ReactNode;
 }
 
-export default function FooterLink({ href, title, rel, target, className, children }: ILinkProps) {
+export default function FooterLink({ href, title, rel, target, className, children }: ILinkProps) :JSX.Element {
   return (
-    <a href={href} title={title} rel={rel} target={target} className={`${styles[className!]}`}>
+    <a href={href} title={title} rel={rel} target={target} className={`${styles[className||""]}`}>
       {children}
     </a>
   );
--- src/js/components/interactive/Link/Link.js
+++ src/js/components/interactive/Link/Link.js
@@ -7,6 +7,6 @@ var react_1 = __importDefault(require("react"));
 var links_module_css_1 = __importDefault(require("./links.module.css"));
 function FooterLink(_a) {
     var href = _a.href, title = _a.title, rel = _a.rel, target = _a.target, className = _a.className, children = _a.children;
-    return (react_1.default.createElement("a", { href: href, title: title, rel: rel, target: target, className: "".concat(links_module_css_1.default[className]) }, children));
+    return (react_1.default.createElement("a", { href: href, title: title, rel: rel, target: target, className: "".concat(links_module_css_1.default[className || ""]) }, children));
 }
 exports.default = FooterLink;
