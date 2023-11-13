a518501927c3300fd82c0db7429171ab81b20b8a
Lucas Sigoli <lucasviniciussigoli@gmail.com>
2023-11-13 17:46:17 -0300

    fix: fix styles bugs on page components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
   title: "Página Incial ",
   description:
     "HomePage do grupo PET/ADS do Instituto Federal de Ciência e Tecnologia do Estado de São Paulo Campus São Carlos",
+
 };
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
