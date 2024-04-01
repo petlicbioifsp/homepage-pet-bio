786c5e2185148d57550ff9c6c27bab8c2c170d4b
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-04-01 12:17:43 -0300

    resolvendo bug no dropDown

--- src/components/menus/MenuMobile/Subcomponents/DropDown.tsx
+++ src/components/menus/MenuMobile/Subcomponents/DropDown.tsx
@@ -2,7 +2,7 @@ import Dot from "./Dot";
 import React from "react";
 import ItemList from "./ItemList";
 import ShadowCut from "./Shadowcut";
-import ContainerCut from "./ConteinerCut"
+import ContainerCut from "./COnteinerCut"
 import { IMenuItem } from "../../../../types/MenuTypes";
 export default function DropdownContent({ items, onClick }: { items: IMenuItem[]; onClick: () => void }) {
     return (
