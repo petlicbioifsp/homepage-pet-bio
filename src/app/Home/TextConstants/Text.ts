b9ae33701608b0e92f136708501fa0b953d60c58
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-04-23 22:54:00 -0300

    modificacoes texto home e faq

--- src/app/Home/TextConstants/Text.ts
+++ src/app/Home/TextConstants/Text.ts
@@ -3,15 +3,19 @@ import { IConstText } from "../../../types/ConstantTypes"
 export const AboutPET:IConstText = {
     desktop: `O Programa de Educação Tutorial (PET) é um programa do governo federal 
     brasileiro de estímulo a atividades de pesquisa, ensino e extensão em nível de 
-     graduação. O PET foi criado e implantado em 1979 pela Coordenação de Aperfeiçoamento 
+    graduação. O PET foi criado e implantado em 1979 pela Coordenação de Aperfeiçoamento 
     de Pessoal de Nível Superior - CAPES, com o nome original de Programa Especial de
-    Treinamento, cujo objetivo era criar um grupo de elite intelectual, dedicado à pesq uisa 
+    Treinamento, cujo objetivo era criar um grupo de elite intelectual, dedicado à pesquisa 
     e ao estudo. Em 2004, o nome foi alterado para Programa de Educação Tutorial e adotou uma 
     nova filosofia. Os objetivos do programa, de acordo com o SESu, são a melhoria do ensino de 
     graduação, a formação acadêmica do aluno, a interdisciplinaridade, e o planejamento e 
     execução de atividades acadêmicas diversificadas em grupos de tutoria. De 2006 a 2012  
     ocorreu uma grande expansão dos grupos PET no país. Atualmente, existem 842 grupos distribuídos 
-    em 121 Instituições de Ensino Superior.`,
+    em 121 Instituições de Ensino Superior.
+    Aqui, no nosso site do PET vinculado ao curso de Licenciatura em Ciências Biológicas do IFSP,
+    você encontrará informações valiosas sobre essa iniciativa enriquecedora que engloba ensino, 
+    pesquisa, extensão e desenvolvimento de habilidades profissionais, 
+    moldando o futuro de estudantes apaixonados pela Biologia. `,
     
     mobile :   `O Programa de Educação Tutorial (PET) é um programa do governo federal
     brasileiro de estímulo a atividades de pesquisa, ensino e extensão em nível de
@@ -19,24 +23,30 @@ export const AboutPET:IConstText = {
     graduação, a formação acadêmica do aluno, a interdisciplinaridade, e o planejamento e
     execução de atividades acadêmicas diversificadas em grupos de tutoria. De 2006 a 2012
     ocorreu uma grande expansão dos grupos PET no país. Atualmente, existem 842 grupos distribuídos
-    em 121 Instituições de Ensino Superior.`
+    em 121 Instituições de Ensino Superior.
+    Aqui, no nosso site do PET vinculado ao curso de Licenciatura em Ciências Biológicas do IFSP,
+    você encontrará informações valiosas sobre essa iniciativa enriquecedora que engloba ensino, 
+    pesquisa, extensão e desenvolvimento de habilidades profissionais, 
+    moldando o futuro de estudantes apaixonados pela Biologia. `
 }
 
 export const Purpose:IConstText = {
     desktop : `O grupo PET do curso de Licenciatura de Ciências Biológicas do IFSP de São Paulo (PET/LICBIO/SPO) 
     foi criado em 2010. Desde sua criação, o PET/LICBIO/SPO já desenvolveu diversas atividades voltadas 
     ao ensino, à pesquisa e à extensão. O grupo é formado por um docente tutor, até doze bolsistas e até seis 
-    voluntários. Cada estudante do PET possui uma dedicação semanal de 20h e atua simultaneamente em diferentes
-    atividades, que vão desde produções de materiais didáticos de biologia para deficientes visuais, até o desenvolvimento
-    de cursos e a organização de eventos acadêmicos. Os estudantes também se envolvem em projetos de extensão e
-    de pesquisa tanto locais quanto em parceria com outras instituições de ensino. Desde 2010, o PET/LICBIO/SPO
-    teve como tutor a docente Martha Cristina Motta Godinho Netto`,
+    voluntários. Cada estudante do PET possui uma dedicação semanal de 20h, os participantes têm a oportunidade de se 
+    envolver em projetos inovadores que não só fortalecem sua base teórica, mas também os capacitam a aplicar seus 
+    conhecimentos em situações práticas, colaborando com a comunidade. o PETLICBIO proporciona um ambiente propício para 
+    o desenvolvimento de habilidades profissionais essenciais, como liderança, comunicação eficaz, trabalho em equipe e pensamento crítico.
+    Os participantes são incentivados a assumir papéis de liderança em projetos de pesquisa e extensão, preparando-os para enfrentar desafios
+    do mundo acadêmico e do mercado de trabalho. Atualmente, o grupo conta com a orientação dedicada da Professora Caroline Arantes Magalhães. 
+    Venha fazer parte desta comunidade comprometida com a excelência acadêmica e o desenvolvimento profissional.
+    `,
     
     
     mobile:  `O grupo PET do curso LICBIO do IFSP São Paulo (PET/LICBIO/SPO) foi criado em 2010. O grupo é formado por um docente
     tutor, até doze bolsistas e até seis voluntários. Cada estudante do PET possui uma dedicação semanal de 20h e atua
-    simultaneamente em diferentes atividades, produções de materiais didáticos para deficientes visuais, até o desenvolvimento
-    de cursos e a organização de eventos acadêmicos. Os estudantes também se envolvem em
-    projetos de extensão e de pesquisa tanto locais quanto em parceria com outras instituições de ensino. Atualmente,
-    o grupo é coordenado pela docente Martha Cristina Motta Godinho Netto`
+    simultaneamente em diferentes atividades, simultaneamente em diferentes atividades, que engloba ensino, pesquisa, extensão os participantes têm a oportunidade de se 
+    envolver em projetos inovadores que não só fortalecem sua base teórica, mas também os capacitam a aplicar seus 
+    conhecimentos em situações práticas, colaborando com a comunidade. Atualmente, o grupo é coordenado pela dedicada Professora Caroline Arantes Magalhães`
 }
