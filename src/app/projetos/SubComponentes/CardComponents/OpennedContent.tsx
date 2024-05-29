95635a801f149abebb43de9538aa026455a024f4
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-05-28 22:59:46 -0300

    Adjusting CSS to position the open cards div

--- src/app/projetos/SubComponentes/CardComponents/OpennedContent.tsx
+++ src/app/projetos/SubComponentes/CardComponents/OpennedContent.tsx
@@ -6,7 +6,7 @@ import Carousel from "../CarouselComponent/Carousel";
 
 export default function OpennedContent({titulo, status, expandido, images}:IOpennedContentProps){ 
     return(
-        <motion.div>
+        <motion.div className="content-card-aberto">
               <motion.h2 className="card-titulo">
                 {titulo} → {status}
               </motion.h2>
