9a01692a1dd59deae094a8fc0075f71dfb8321d8
Fhelippe <fhelippe.d@aluno.ifsp.edu.br>
2024-08-27 12:58:24 -0300

    feat: inscription requirements are passed as an array on processos.json

--- src/app/processoSeletivo/SubComponentes/Requisitos/Requisitos.tsx
+++ src/app/processoSeletivo/SubComponentes/Requisitos/Requisitos.tsx
@@ -1,18 +1,24 @@
 "use client";
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./Requisitos.module.css";
 import useFetchProcessos from "@/hooks/fetch/useFetchProcessos";
 
 export default function Requisitos() {
   const { requisitos } = useFetchProcessos();
+
+    console.log(requisitos);
+
   return (
     <div className={styles.requisitos}>
       <h2 className={styles.titulo_secundario}>Requisitos</h2>
       <p className={styles.texto}>{requisitos?.introducao}</p>
       <ul>
-        <li className={styles.texto}>{requisitos?.item_1}</li>
-        <li className={styles.texto}>{requisitos?.item_2}</li>
-        <li className={styles.texto}>{requisitos?.item_3}</li>
+        { requisitos && requisitos?.requirements.length > 0 ? 
+        
+          requisitos.requirements.map( (item: {text: string}, index: number) => <li key={index} className={styles.texto}>{item.text}</li> ) : 
+            
+            (<p>Não há requisitos</p>)
+        }
       </ul>
     </div>
   );
