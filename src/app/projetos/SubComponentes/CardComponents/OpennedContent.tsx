f1e2c90d65f8173e2b7dd6b8cbb11fada6f3416e
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-09-09 09:50:10 -0300

    feat: implement new template for project card on mobile devices

--- src/app/projetos/SubComponentes/CardComponents/OpennedContent.tsx
+++ src/app/projetos/SubComponentes/CardComponents/OpennedContent.tsx
@@ -1,25 +1,44 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { IOpennedContentProps} from "../../../../types/ProjectTypes";
-import '../CardProjeto/cardProjeto.css'
+import { IOpennedContentProps } from "../../../../types/ProjectTypes";
+import "../CardProjeto/cardProjeto.css";
 import Carousel from "../CarouselComponent/Carousel";
+import useIsMobile from "@/hooks/window/useIsMobile";
 
-export default function OpennedContent({titulo, status, expandido, images}:IOpennedContentProps){ 
-    return(
+export default function OpennedContent({
+  titulo,
+  status,
+  expandido,
+  images,
+}: IOpennedContentProps) {
+  const isMobile: boolean = useIsMobile();
+  return (
+    <>
+      {isMobile ? (
         <motion.div className="content-card-aberto">
-              <motion.h2 className="card-titulo">
-                {titulo} → {status}
-              </motion.h2>
-              <motion.div className="scrowSpace">
-                <p className="card--texto__expandido">{expandido}</p>
-                {/* <div className="card--texto__envolvidos"> */}
-                  {/* <p className="card--texto__expandido">{tecnologias} </p> */}
-                  {/* <p className="card--texto__expandido"></p> */}
-                {/* </div> */}
-                <div className="carousel">
-                  <Carousel images={images}/>
-                </div>
-              </motion.div>
-            </motion.div>
-    )
+          <div className="carousel">
+            <Carousel images={images} />
+          </div>
+          <motion.h2 className="card-titulo">
+            {titulo} → {status}
+          </motion.h2>
+          <motion.div className="scrowSpace">
+            <p className="card--texto__expandido">{expandido}</p>
+          </motion.div>
+        </motion.div>
+      ) : (
+        <motion.div className="content-card-aberto">
+          <motion.h2 className="card-titulo">
+            {titulo} → {status}
+          </motion.h2>
+          <motion.div className="scrowSpace">
+            <p className="card--texto__expandido">{expandido}</p>
+            <div className="carousel">
+              <Carousel images={images} />
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </>
+  );
 }
