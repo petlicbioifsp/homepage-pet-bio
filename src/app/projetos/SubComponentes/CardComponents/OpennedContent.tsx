7458c7e25e1c023c7fe89cc699d48263cdc0154e
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-09-16 14:26:32 -0300

    feat: new template application for project card

--- src/app/projetos/SubComponentes/CardComponents/OpennedContent.tsx
+++ src/app/projetos/SubComponentes/CardComponents/OpennedContent.tsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import { IOpennedContentProps } from "../../../../types/ProjectTypes";
 import "../CardProjeto/cardProjeto.css";
 import Carousel from "../CarouselComponent/Carousel";
-import useIsMobile from "@/hooks/window/useIsMobile";
 
 export default function OpennedContent({
   titulo,
@@ -11,22 +10,8 @@ export default function OpennedContent({
   expandido,
   images,
 }: IOpennedContentProps) {
-  const isMobile: boolean = useIsMobile();
   return (
     <>
-      {isMobile ? (
-        <motion.div className="content-card-aberto">
-          <div className="carousel">
-            <Carousel images={images} />
-          </div>
-          <motion.h2 className="card-titulo">
-            {titulo} → {status}
-          </motion.h2>
-          <motion.div className="scrowSpace">
-            <p className="card--texto__expandido">{expandido}</p>
-          </motion.div>
-        </motion.div>
-      ) : (
         <motion.div className="content-card-aberto">
           <motion.h2 className="card-titulo">
             {titulo} → {status}
@@ -38,7 +23,6 @@ export default function OpennedContent({
             </div>
           </motion.div>
         </motion.div>
-      )}
     </>
   );
 }
