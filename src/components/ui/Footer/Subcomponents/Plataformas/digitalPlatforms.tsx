f60852b1d57da18d7a8e76736399b6fab7ea1002
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2024-09-24 09:17:14 -0300

    feat: styles for digital platforms component and footer

---
+++ src/components/ui/Footer/Subcomponents/Plataformas/digitalPlatforms.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "../../../../Link/Link";
+import useFetchPlataformas from "@/hooks/fetch/useFetchPlataformas";
+import styles from "./digitalPlataforms.module.css";
+
+export default function DigitalPlatforms() {
+  return useFetchPlataformas({
+    render: (plataformas) => (
+      <div className={styles.platforms_content}>
+        <h3 className={styles.text_section}>Plataformas</h3>
+        <ul className={styles.list_platforms}>
+          {plataformas.map((item) => (
+            <li className={styles.link} key={item.id}>
+              <Link
+                className="link"
+                href={item.url}
+                children={item.nome}
+                rel="noreferrer"
+                target="_blank"
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    ),
+  });
+}
--- src/components/ui/Footer/footer.tsx
+++ src/components/ui/Footer/footer.tsx
@@ -2,36 +2,44 @@ import React from "react";
 import styles from "./footer.module.css";
 import { FaGithub } from "react-icons/fa";
 import Link from "../../Link/Link";
+import DigitalPlatforms from "./Subcomponents/Plataformas/digitalPlatforms";
 
-export default function Footer(){
+export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Link
-        className="gitIcon"
-        href="https://github.com/pet-ads"
-        title="Visite nosso github"
-        rel="noreferrer"
-        target="_blank"
-      >
-        <FaGithub />
-      </Link>
-
-      <pre className={styles.footer_content}>
-        Desenvolvido com ♥ por{" "}
-        <Link className="link" href="http://petads.paginas.scl.ifsp.edu.br/" title="Página inicial">
-          PET/ADS
-        </Link>{" "}
-        @{" "}
+      <DigitalPlatforms />
+      <>
         <Link
-          className="link"
-          href="https://scl.ifsp.edu.br"
-          title="Página do IFSP-SCL"
+          className="gitIcon"
+          href="https://github.com/pet-ads"
+          title="Visite nosso github"
           rel="noreferrer"
           target="_blank"
         >
-          IFSP São Carlos.
+          <FaGithub />
         </Link>
-      </pre>
+
+        <pre className={styles.footer_content}>
+          Desenvolvido com ♥ por{" "}
+          <Link
+            className="link"
+            href="http://petads.paginas.scl.ifsp.edu.br/"
+            title="Página inicial"
+          >
+            PET/ADS
+          </Link>{" "}
+          @{" "}
+          <Link
+            className="link"
+            href="https://scl.ifsp.edu.br"
+            title="Página do IFSP-SCL"
+            rel="noreferrer"
+            target="_blank"
+          >
+            IFSP São Carlos.
+          </Link>
+        </pre>
+      </>
     </footer>
   );
 }
