4447a9dbbd24358d50bda556be004efe303e1c80
PET Licenciatura Biologia <petlicbioifsp@gmail.com>
2025-09-10 10:35:44 -0300

    Update digitalPlatforms.tsx

--- src/components/ui/Footer/Subcomponents/Plataformas/digitalPlatforms.tsx
+++ src/components/ui/Footer/Subcomponents/Plataformas/digitalPlatforms.tsx
@@ -4,24 +4,37 @@ import Link from "../../../../Link/Link";
 import useFetchPlataformas from "@/hooks/fetch/useFetchPlataformas";
 import styles from "./digitalPlataforms.module.css";
 
+//importa todos que vai usar
+import { FaInstagram, FaYoutube, FaGlobe } from "react-icons/fa";
+
+// cria um "mapa" de strings -> componente
+const icons = {
+  FaInstagram: FaInstagram,
+  FaYoutube: FaYoutube,
+  FaGlobe: FaGlobe,
+};
+
 export default function DigitalPlatforms() {
   return useFetchPlataformas({
     render: (plataformas) => (
       <div className={styles.platforms_content}>
         <h3 className={styles.text_section}>Plataformas</h3>
         <ul className={styles.list_platforms}>
-          {plataformas.map((item) => (
-            <li className={styles.link} key={item.id}>
-              <Link
-                className="link"
-                href={item.url}
-                rel="noreferrer"
-                target="_blank"
-              >
-                {item.nome}
-              </Link>
-            </li>
-          ))}
+          {plataformas.map((item) => {
+            const Icon = icons[item.icon]; // pega o ícone correto
+            return (
+              <li className={styles.link} key={item.id}>
+                <Link
+                  className="link"
+                  href={item.url}
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  {Icon && <Icon className={styles.icon} />} {item.nome}
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     ),
